3b3e279b76fe5ed54bdd31ed083813a8e0639b76
Miles Calloway <26465212+mcallbosco@users.noreply.github.com>
2023-05-22 00:00:06 -0400

    Simplification
    Co-authored-by: Jason Wen <haibin.wen3@gmail.com>

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -376,14 +376,7 @@ static const addr_checks* honda_nidec_init(uint16_t param) {
   honda_alt_brake_msg = false;
   honda_bosch_long = false;
   honda_bosch_radarless = false;
- 
-  
-  if (GET_FLAG(param, HONDA_PARAM_CLARITY)){
-    honda_clarity_brake_msg = true;
-    print("Honda Clarity Brake Messages Active\n");
-  } else {
-    honda_clarity_brake_msg = false;
-  }
+  honda_clarity_brake_msg = GET_FLAG(param, HONDA_PARAM_CLARITY);
   if (GET_FLAG(param, HONDA_PARAM_NIDEC_ALT)) {
     honda_rx_checks = (addr_checks){honda_nidec_alt_addr_checks, HONDA_NIDEC_ALT_ADDR_CHECKS_LEN};
   } else {
