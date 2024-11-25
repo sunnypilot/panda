6e5e967f224c3c334569cac21400a46447f45a2b
Jason Wen <haibin.wen3@gmail.com>
2024-11-25 12:39:03 -0500

    probably needed

--- tests/safety/common.py
+++ tests/safety/common.py
@@ -1010,6 +1010,8 @@ class PandaCarSafetyTest(PandaSafetyTest):
   def test_safety_tick(self):
     self.safety.set_timer(int(2e6))
     self.safety.set_controls_allowed(True)
+    self.safety.set_controls_allowed_lat(True)
     self.safety.safety_tick_current_safety_config()
     self.assertFalse(self.safety.get_controls_allowed())
+    self.assertFalse(self.safety.get_controls_allowed_lat())
     self.assertFalse(self.safety.safety_config_valid())
