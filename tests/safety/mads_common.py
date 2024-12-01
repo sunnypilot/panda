28150c602364fc4e442bea497953b6e417a07626
DevTekVE <devtekve@gmail.com>
2024-12-02 00:26:27 +0100

    adding some more tess

--- tests/safety/mads_common.py
+++ tests/safety/mads_common.py
@@ -20,27 +20,27 @@ class MadsCommonBase(unittest.TestCase):
                         self.safety.set_main_button_prev(main_button_prev)
                         self.safety.set_lkas_button_prev(-1) # Force to not have LKAS
                         self._rx(self._user_brake_msg(False))
-                        self.assertEqual(enable_mads and main_button_prev == 1, self.safety.get_controls_allowed_lat(),
-                                         (f"main_button_prev: [{self.safety.get_main_button_prev()}] | " +
-                                          f"mads_state_flags: [{self.safety.get_mads_state_flags()}: {bin(self.safety.get_mads_state_flags())}] | " +
-                                          f"main_transition: [{self.safety.get_mads_main_button_transition()}], " +
-                                          f"cur [{self.safety.get_mads_main_button_current()}], " +
-                                          f"last [{self.safety.get_mads_main_button_last()}]"))
+                        self.assertEqual(enable_mads and main_button_prev == 1, self.safety.get_controls_allowed_lat())
 
-    # def test_enable_control_from_main(self):
-    #     for enable_mads in (True, False):
-    #         with self.subTest("enable_mads", mads_enabled=enable_mads):
-    #             self.safety.set_enable_mads(enable_mads, False)
-    #             for acc_state_msg_valid in (True, False):
-    #                 with self.subTest("acc_state_msg", state_valid=acc_state_msg_valid):
-    #                     msg = self._acc_state_msg(acc_state_msg_valid)
-    #                     self._rx(msg)
-    #                     self.assertEqual(acc_state_msg_valid, self.safety.get_acc_main_on(), f"msg: {hex(msg.addr)}")
+    def test_enable_control_from_lkas_button_prev(self):
+        for enable_mads in (True, False):
+            with self.subTest("enable_mads", mads_enabled=enable_mads):
+                self.safety.set_enable_mads(enable_mads, False)
+                for lkas_button_prev in (-1, 0, 1):
+                    with self.subTest("lkas_button_prev", button_state=lkas_button_prev):
+                        self.safety.set_main_button_prev(-1)
+                        self.safety.set_lkas_button_prev(lkas_button_prev)
+                        self._rx(self._user_brake_msg(False))
+                        self.assertEqual(enable_mads and lkas_button_prev == 1, self.safety.get_controls_allowed_lat())
 
-    # def test_lkas_button(self):
-    #     for enable_mads in (True, False):
-    #         with self.subTest("enable_mads", enable_mads=enable_mads):
-    #             self.safety.set_enable_mads(enable_mads)
-    #             self.safety.set_controls_allowed_lat(False)
-    #             self._rx(self._lkas_button_msg(enable_mads))
-    #             self.assertEqual(enable_mads, self.safety.get_is_lat_active())
+    def test_mads_state_flags(self):
+        for enable_mads in (True, False):
+            with self.subTest("enable_mads", mads_enabled=enable_mads):
+                self.safety.set_enable_mads(enable_mads, False)
+                self.safety.set_main_button_prev(0)
+                self.safety.set_lkas_button_prev(0)
+                self._rx(self._user_brake_msg(False))
+                self.assertTrue(self.safety.get_mads_state_flags() & 2)
+                self.assertTrue(self.safety.get_mads_state_flags() & 4)
+        self.safety.set_main_button_prev(-1)
+        self.safety.set_lkas_button_prev(-1)
