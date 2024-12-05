4ed88adf36c3823703798cf35e364ce305883988
Jason Wen <haibin.wen3@gmail.com>
2024-12-05 18:25:53 -0500

    match yota

--- board/safety/safety_subaru.h
+++ board/safety/safety_subaru.h
@@ -107,8 +107,8 @@ static void subaru_rx_hook(const CANPacket_t *to_push) {
   }
 
   if ((addr == MSG_SUBARU_ES_LKAS_State) && (bus == SUBARU_CAM_BUS)) {
-    int lkas_hud = (GET_BYTE(to_push, 2) & 0x0CU) >> 2U;
-    if ((lkas_hud >= 1) && (lkas_hud <= 3)) {
+    bool lkas_hud = ((GET_BYTE(to_push, 2) & 0x0CU) >> 2U) > 0U;
+    if (lkas_hud) {
       lkas_button_press = MADS_BUTTON_PRESSED;
     }
   }
